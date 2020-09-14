f6bd94fe664e2c1a7dd416a3aea6da2bf1d2d8f8
fgremler <32358312+fgremler@users.noreply.github.com>
2020-09-14 14:31:26 +0200

    use warcprox

--- facebook_harvester.py
+++ facebook_harvester.py
@@ -15,7 +15,7 @@ class FacebookHarvester(BaseHarvester):
 
     def __init__(self, working_path, stream_restart_interval_secs=30 * 60, mq_config=None,debug=False,
                  connection_errors=5, http_errors=5, debug_warcprox=False, tries=3):
-        BaseHarvester.__init__(self, working_path, mq_config=mq_config, use_warcprox = False,
+        BaseHarvester.__init__(self, working_path, mq_config=mq_config, use_warcprox = True,
                                stream_restart_interval_secs=stream_restart_interval_secs,
                                debug=debug, debug_warcprox=debug_warcprox, tries=tries)
 
